57eddf83986d26d2a8a352c38c896685d658d33d
AkForeva <61966032+AkForeva@users.noreply.github.com>
2021-01-20 16:04:00 +0545

    added cube movement

---
+++ My Task/Assets/Scripts/Cube.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    bool repeatSequence;
+    bool stopMovement;
+    bool isPaused;
+
+    string previousPoint;
+
+    Vector3 pointA = new Vector3(-2, 0, -6);
+    Vector3 pointB = new Vector3(2, 0, -6);
+    Vector3 pointC = new Vector3(0, 0, 0);
+
+    Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousPoint = "a";
+        repeatSequence = false;
+        stopMovement = false;
+        targetPosition = pointB;
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!stopMovement && !isPaused)
+        {
+            MoveToTarget();
+            CheckPosition();
+        }
+    }
+
+    void MoveToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 3);
+    }
+
+    void CheckPosition()
+    {
+        if (transform.position == pointB)
+        {
+            if(previousPoint == "a")
+            {
+                targetPosition = pointC;
+            }
+            else if(previousPoint == "c")
+            {
+                targetPosition = pointA;
+            }
+            stopMovement = true;
+            StartCoroutine(EnableMovement());
+        }
+        else if (transform.position == pointC)
+        {
+            targetPosition = pointB;
+            previousPoint = "c";
+        }
+        else if(transform.position == pointA)
+        {
+            targetPosition = pointB;
+            previousPoint = "a";
+        }
+    }
+
+    IEnumerator EnableMovement()
+    {
+        yield return new WaitForSeconds(3);
+        stopMovement = false;
+    }
+
+    public void OnPausePressed()
+    {
+        isPaused = !isPaused;
+    }
+}
---
+++ My Task/Assets/Scripts/CubeManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public Action OnPauseClicked;
+    public GameObject cubePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void SpawnCube()
+    {
+        GameObject cube = GameObject.Instantiate(cubePrefab, transform.position, transform.rotation);
+        cube.transform.position = new Vector3(-2, 0, -6);
+        OnPauseClicked += cube.GetComponent<Cube>().OnPausePressed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown("return"))
+        {
+            SpawnCube();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            OnPauseClicked();
+        }
+    }
+}
